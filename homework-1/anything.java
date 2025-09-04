ddd457e5000c645dfce4cabc0772cc10eb596449
an0n-ambrozio <matheus@ambrozio.space>
2025-09-04 00:20:38 +0000

    Organizin my repo

--- homework-1/anything.java
+++
@@ -1,5 +0,0 @@
-public class anything {
-    public static void main(String[] args) {
-        System.out.println("Success comes to those who never give up!");
-    }
-}
---
+++ hw1/Cipher.java
@@ -0,0 +1,32 @@
+package hw1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Cipher {
+
+    public static void main(String[] args) {
+        int m = (int)Math.pow(2, 7);
+        char[] cipher = new char[m];
+        for (int i = 0; i < m; i++)
+            cipher[i] = (char)mod(55 * i + 91, m);
+        String plain = read("/workspaces/DataStructure/hw1/media/cipher.txt");
+        for (int i = 0; i < plain.length(); i++)
+            System.out.print(cipher[plain.charAt(i)]);
+    }
+
+    public static String read(String path) {
+        String ret = null;
+        try {
+            ret = new String(Files.readAllBytes(Paths.get(path)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public static int mod(int x, int n) {
+        return ((x % n) + n) % n;
+    }
+}
